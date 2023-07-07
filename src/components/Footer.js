f97667fffc86ea6a02a64bda788d77156185c627
Jarod Kober <jarod.development@gmail.com>
2023-07-07 15:30:42 -0700

    Email Address in Footer
    * Added an envelope icon contains a mailto link
    * Obfuscated the email address with base64 encoding that only populates the href once the user clicks the icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsEnvelope, BsGithub, BsLinkedin } from 'react-icons/bs';
 
 function Footer() {
+	const handleClick = () => {
+		const encEmail = 'amFyb2QuZGV2ZWxvcG1lbnRAZ21haWwuY29t';
+
+		const el = document.getElementById('contact');
+
+		el.setAttribute('href', 'mailto:'.concat(atob(encEmail)));
+	};
+
 	return (
 		<footer>
+			<a
+				href="obfuscated"
+				id="contact"
+				onClick={handleClick}
+			>
+				<BsEnvelope />
+			</a>
 			<a
 				href="https://github.com/jarodkober"
 				rel="noreferrer"
