76a2cb06e3046421bcb75bed5316849933ed547a
Jarod Kober <jarod.development@gmail.com>
2023-07-07 13:41:47 -0700

    Implemented Projects Table
    * Added a Table component to the Projects page
    * Defined project table config, data, and key

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,9 +1,53 @@
 import Table from '../components/Table';
 
 function ProjectsPage() {
+	const config = [
+		{ label: 'Name', render: (project) => project.name },
+		{
+			label: 'URL',
+			render: (project) => (
+				<a
+					href={project.url}
+					rel="noreferrer"
+					target="_blank"
+				>
+					{project.url}
+				</a>
+			)
+		},
+		{
+			label: 'Repository',
+			render: (project) => (
+				<a
+					href={project.repository_url}
+					rel="noreferrer"
+					target="_blank"
+				>
+					Github
+				</a>
+			)
+		}
+	];
+
+	const data = [
+		{
+			name: 'Portfolio',
+			repository_url: 'https://github.com/jarodkober/portfolio',
+			url: 'http://jarodkober.com'
+		}
+	];
+
+	const keyFn = (project) => {
+		return project.name;
+	};
 	return (
 		<div>
 			<h1>Projects</h1>
+			<Table
+				config={config}
+				data={data}
+				keyFn={keyFn}
+			/>
 		</div>
 	);
 }
