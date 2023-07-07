5aa41150e51fac54cb20d0bea72be7433a5c2a31
Jarod Kober <jarod.development@gmail.com>
2023-07-07 14:01:41 -0700

    Introduction First Draft
    * Added an initial draft of the introduction copy in the HomePage component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,21 @@
 function HomePage() {
-	return <div>Home Page</div>;
+	return (
+		<div>
+			<p>
+				Hello! I&rsquo;m Jarod Kober, a web developer specializing in
+				front end development, with 14 years experience building and
+				leading development of highly performant websites and web
+				applications.
+			</p>
+			<p>
+				I'm currently seeking a new opportunity as a
+				front-of-the-front-end specialist, delivering expertise in HTML,
+				CSS, and presentational JavaScript to create UIs that are
+				performant, responsive, accessible, flexible, and scalable.
+				Design systems anyone?
+			</p>
+		</div>
+	);
 }
 
 export default HomePage;
