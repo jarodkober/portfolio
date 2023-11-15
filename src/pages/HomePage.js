030125e74c5cf0a1b7c2de2fbeadfa73947eb5a6
Jarod Kober <jarod.development@gmail.com>
2023-11-15 14:42:17 -0700

    Home Page Content Update * Updated the home page content to be more closely aligned with my resume summary

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,17 +5,17 @@ function HomePage() {
 		<Panel>
 			<p>
 				Hello! I&rsquo;m Jarod Kober, a web developer specializing in
-				front end development, with 14 years experience building and
-				leading development of highly performant websites and web
-				applications.
+				front&ndash;end development, with 14 years experience building
+				and leading development of highly performant web applications.
 			</p>
 			<p>
 				I'm currently seeking a new opportunity as a
 				front-of-the-front-end specialist, delivering expertise in HTML,
-				CSS, and presentational JavaScript to create user interfaces
-				that are performant, responsive, accessible, flexible, and
-				scalable. Design systems anyone?
+				CSS, and presentational JavaScript to create accessible,
+				flexible, and scalable user interfaces with a meaningful user
+				experience.
 			</p>
+			<p>Design systems anyone?</p>
 		</Panel>
 	);
 }
