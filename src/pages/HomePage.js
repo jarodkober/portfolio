cb43f46923ecc2952e28f32401a87bbb300c4943
Jarod Kober <jarod.development@gmail.com>
2023-11-16 12:54:38 -0700

    Home Page Content Update * Switched "meaningful" to "seamless"...sounds a little less pretentious, but still reflects the intention behind what I build

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
 				I'm currently seeking a new opportunity as a
 				front-of-the-front-end specialist, delivering expertise in HTML,
 				CSS, and presentational JavaScript to create accessible,
-				flexible, and scalable user interfaces with a meaningful user
+				flexible, and scalable user interfaces with a seamless user
 				experience.
 			</p>
 			<p>Design systems anyone?</p>
